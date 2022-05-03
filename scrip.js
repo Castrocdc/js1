9cf03d6028d96be967fc38e13318fa79a58b8132
castrocdc <livcdc@gmail.com>
2022-05-02 23:09:51 -0300

    proyecto final en desarrollo

---
+++ scrip.js
@@ -0,0 +1,33 @@
+const laHora = 300
+function Alumno (nombre, apellido, edad, pagoCuota){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.pagoCuota = pagoCuota;
+  }
+let array = [alumnos]
+console.log (array)
+function cuidado(laHora) {
+  let guarderia = prompt ("ingrese horas de cuidado por dia")
+
+   guarderia = guarderia * laHora * 20
+    alert ("su cuota es de " + guarderia + " pesos al mes")
+
+       
+}
+niño = prompt ("ingrese la edad del niñ@")
+
+if (niño <= 5){
+
+ cuidado (laHora)
+ 
+}
+else alert ("el niñ@ es mayor a lo requerido")
+
+Alumno (prompt ("ingrese nombre:"), prompt("ingrese apellido:"), prompt("ingrese edad:"), prompt("tiene la untima cuota paga?:"));
+
+if (this.pagoCuota == "si"){
+  alert ("aprobado")
+} else {
+  alert("no pago la cuota")
+} 
